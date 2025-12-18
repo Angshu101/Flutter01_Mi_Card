170f2b81e55895325ba21ae37ebf09c313cca12a
Angshu101 <36377710+Angshu101@users.noreply.github.com>
2025-12-18 21:08:18 +0530

    First Challange Completed

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,45 @@ class MyCardApp extends StatelessWidget {
           backgroundColor: Colors.blueGrey[900],
         ),
         backgroundColor: Colors.teal,
-        body: Container(
+        // Container try's to get as big as possible unless we provie a child and that container occupies a child's space
+       // Container Can only have one child , margin is for outside padding for inside
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:<Widget>[
+              Container(
+
+                color: Colors.red,
+                width: 100,
+                height: 100,
+
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:<Widget> [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100,
+                    height: 100,
+                  
+                  ),
+                   Container(
+                    color: Colors.green,
+                    width: 100,
+                    height: 100,
+                  
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+
+              ),
+            ],
+          ),
         ),
       )
     );
